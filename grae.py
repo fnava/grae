a9d6d7047bb516dd22f29f074b38389f938aa5ff
fortu <fortu@localhost.localdomain>
2016-05-11 01:08:17 +0200

    Parse and compile all shard filters responses. Build magnitudes array for a given spectra

--- grae.py
+++ grae.py
@@ -3,6 +3,8 @@
 import pysysp
 import pyfits
 
+import numpy as np
+
 #load a spectrum from a fits file
 #vega = pysysp.StarSpectrum('alpha_lyr_stis_006.fits')
 
@@ -45,7 +47,7 @@ miles_spectra = pysysp.StarSpectrum(synt)
 miles_params = miles_parse(synt)
 
 #print the flux
-print(miles_spectra.flux)
+#print(miles_spectra.flux)
 
 #Check available filters
 #print(pysysp.showfilters)
@@ -57,8 +59,32 @@ print(miles_spectra.flux)
 #print(B)
 
 #You can also load any ascii file with two columns (first wavelength, second filter response)
-S = pysysp.BandPass('../shards/shards_f602w17.res')
-print(miles_spectra.apmag(S,mag='AB',mzero=0.0))
-print(miles_spectra.apmag(S,mag='Vega',mzero=0.03))
+#S = pysysp.BandPass('../shards/shards_f602w17.res')
+#print(miles_spectra.apmag(S,mag='AB',mzero=0.0))
+#print(miles_spectra.apmag(S,mag='Vega',mzero=0.03))
 
+# Parse all SHARD band responses:
+from pathlib import Path,PurePosixPath
+def parse_shard():
+	shards_band = []
+	shards_path = Path('../shards/')
+	for shard_file in shards_path.iterdir(): 
+		if shard_file.is_file() and PurePosixPath(shard_file).suffix=='.res':
+			shards_band.append(pysysp.BandPass('../shards/%s' % shard_file))
+	shards = sorted(shards_band, key=lambda b: np.max(b.wavelength))
+	return shards
 
+# Compute band magnitudes for a given spectra
+def compute_response(shards, spectra):
+	resp=[]
+	for band in shards:
+		mag=0
+		try:	
+			mag=spectra.apmag(band,mag='AB',mzero=0.0)
+		except ValueError:
+			pass
+		resp.append((np.max(band.wavelength),mag))
+	for a,b in resp: print(a,b)
+
+# Put all together
+compute_response(parse_shard(), miles_spectra)
